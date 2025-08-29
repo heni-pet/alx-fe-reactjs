70e5b920a19118a143be33f2f0de607a1c0200da
henok <heni.hadush@gmail.com>
2025-08-29 14:14:55 +0300

    updated the file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,16 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header'
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <Header />
       <WelcomeMessage />
+      <Header />
       <MainContent />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
