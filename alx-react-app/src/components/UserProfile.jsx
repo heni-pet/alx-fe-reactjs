6c6f493b63f79e1827ce491d1840fd9195b2f19a
henok <heni.hadush@gmail.com>
2025-08-29 14:31:44 +0300

    updated the file

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 const UserProfile = (props) => {
   return (
     <div style={styles.card}>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>{props.bio}</p>
+      <h1>{props.name}</h1>
+      <h2>Age: {props.age}</h2>
+      <h3>Bio: {props.bio}</h3>
     </div>
   );
 };
