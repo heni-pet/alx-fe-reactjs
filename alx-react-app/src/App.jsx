7472ca2ca03732f536a055279d6fd501d48ee7cf
henok <heni.hadush@gmail.com>
2025-08-29 11:18:26 +0300

    add Header,main and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,14 @@
 import WelcomeMessage from './components/WelcomeMessage'
-
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 function App() {
   return (
     <div>
+      <Header />
       <WelcomeMessage />
+      <MainContent />
+      <Footer />
     </div>
   )
 }
