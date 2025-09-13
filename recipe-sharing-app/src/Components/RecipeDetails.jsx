619a7011cb6449f6129b5c72fd9ae0af90758c28
henok <heni.hadush@gmail.com>
2025-09-13 11:40:56 +0300

    update recipeStore.jsx'

--- recipe-sharing-app/src/Components/RecipeDetails.jsx
+++ recipe-sharing-app/src/Components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 
-import { useRecipeStore } from '../recipeStore';
+import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
 const RecipeDetails = ({ recipeId }) => {
