0f41af30f13e123c221ce00121b19f2821a3bd5a
henok <heni.hadush@gmail.com>
2025-09-13 20:51:24 +0300

    add new functionlity DeleteRecipeButton.jsx

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,15 @@
 import { useRecipeStore } from './recipeStore';
-
+import { useNavigate } from 'react-router-dom';
 export default function DeleteRecipeButton({ recipeId, onDelete }) {
   const deleteRecipe = useRecipeStore((s) => s.deleteRecipe);
+  const navigate = useNavigate();
 
   return (
     <button
       onClick={() => {
         deleteRecipe(recipeId);
         onDelete?.();
+        navigate('/');
       }}
     >
       Delete Recipe
