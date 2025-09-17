bdab3a447fb90ce27b94104b73ab81a1a6166080
henok <heni.hadush@gmail.com>
2025-09-17 17:50:17 +0300

    new file Search.jsx'

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-export async function fetchUserData(username, location, minRepos) {
+export async function fetchUserData(username, searchLocation, minRepos) {
   if (!username) throw new Error("No username provided");
   const url = `https://api.github.com/users/${username}`;
   
-const response = await axios.get(url);
+  const response = await axios.get(url);
 
   return response.data;
 }
