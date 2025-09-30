176299fd7ccd0ad548eeafc7543b64edea1df5ce
henok <heni.hadush@gmail.com>
2025-09-30 11:23:01 +0300

    recipe share add

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
 
   return (
     <main className="p-6 bg-gray-50 min-h-screen">
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-1">
+      <div className="grid gap-6 sm:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map(recipe => (
           <div
             key={recipe.id}
