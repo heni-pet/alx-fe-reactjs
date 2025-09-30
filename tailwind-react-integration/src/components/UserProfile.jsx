06c605be19b3b02bdc2ead42fb17c77f528881d4
henok <heni.hadush@gmail.com>
2025-09-30 09:10:45 +0300

    install tailwind manually config taildwind.config.js

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ function UserProfile() {
         className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4 transition-colors duration-300 ease-in-out hover:text-blue-500">John Doe</h1>
-      <p className="text-sm md:text-base text-gray-600">
+      <p className="text-sm md:text-base text-blue-950">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
