127e891415e7b8604ac10dab084ac195ddd54cc7
henok <heni.hadush@gmail.com>
2025-10-09 09:21:09 +0300

    add string required

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,15 +2,9 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  username: Yup.string()
-    .trim()
-    .required("Username is required"),
-  email: Yup.string()
-    .email("Invalid email format")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
-    .required("Password is required")
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().email("Invalid email format").required("Email is required"),
+  password: Yup.string().required("Password is required")
 });
 
 export default function FormikForm() {
@@ -85,6 +79,3 @@ export default function FormikForm() {
     </div>
   );
 }
-
-
-
