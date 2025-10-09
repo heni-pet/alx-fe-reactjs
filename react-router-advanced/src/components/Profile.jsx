8a262ab896d022d4cd1944fc3957460e5239a475
henok <heni.hadush@gmail.com>
2025-10-09 14:18:40 +0300

    i add broweserrouter,route,router

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { BrowserRouter,Router,Route,Link, Outlet } from "react-router-dom";
 
 export default function Profile() {
   return (
     <div>
       <h2>User Profile</h2>
       <ul>
-        <li><Link to="details">Profile Details</Link></li>
-        <li><Link to="settings">Profile Settings</Link></li>
+        <BrowserRouter>
+          <Route>
+            <li><Link to="details">Profile Details</Link></li>
+            <li><Link to="settings">Profile Settings</Link></li>
+          </Route>
+        </BrowserRouter>
       </ul>
       <Outlet />
     </div>
