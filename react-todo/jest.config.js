508caaa579dd05d64d57b5ed034d9679ba9c2706
henok <heni.hadush@gmail.com>
2025-10-10 13:45:13 +0300

    i test with jest

---
+++ react-todo/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"]
+};
